d620c7c4fda658d56f0fc87e23e43de7cd720bea
Sofia Gaona <sofiabelengaona@outlook.com>
2025-03-18 13:03:11 +0100

    somma

---
+++ somma.py
@@ -0,0 +1,7 @@
+valore_1 = input("Passami il primo valore: ")
+valore_2 = input("Passami il secondo valore: ")
+v1 = int(valore_1)
+v2 = int(valore_2)
+
+sommaTotale =  v1 + v2
+print("la somma di ", v1, "e", v2, "è", sommaTotale)
