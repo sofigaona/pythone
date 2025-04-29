53dd49e177467139edea4092a12348ebafc627a3
sofigaona <sofiabelengaona@outlook.com>
2025-04-29 11:27:43 +0200

    Add files via upload

---
+++ spigolatura.py
@@ -0,0 +1,8 @@
+import urllib.request
+url = "https://www.itsturismomarche.it/"
+risultato = urllib.request.urlopen(url)
+theBytes = risultato.read()
+text = theBytes.decode()
+import bs4
+doc = bs4.BeautifulSoup(text)
+print(doc.prettify())
